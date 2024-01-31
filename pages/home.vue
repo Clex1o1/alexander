d62a1bc4e5da7209684864204904a1e619c28175
Alexander Classen (Westwerk) <ac@westwerk.ac>
2024-01-31 08:34:56 +0100

    fixes

--- pages/home.vue
+++ pages/home.vue
@@ -3,7 +3,7 @@ useHead({
   title: "Alexander",
 });
 definePageMeta({
-  title: "The Great",
+  title: "The Great.dev",
 });
 </script>
 <template>
@@ -12,19 +12,19 @@ definePageMeta({
     <p class="mt-4">I know it's a huge title...</p>
     <p class="mt-4">
       Let me explain: If you know my name and the domain and naming problem
-      everybody has you might understand why I choose it. Also, I have a new
-      email address: alexander@the-great.dev, how cool is that? It could mean
-      'Alexander the Great' or 'Alexander the Great Developer' which would imply
-      that I'm the great? Well, I'm not. I'm just another developer and tech guy
-      who starts late into the blog game. It could also stand for just 'The
-      Great' - singular or plural. Depending on the technologies, programming
-      languages, frameworks and people I will mention. That's fits best!
+      everybody has, you might understand why I chose it. Also, I have a new
+      email address: alexander@the-great.dev. How cool is that? It could mean
+      'Alexander the Great' or 'Alexander the Great Developer,' which would
+      imply that I'm great? Well, I'm not. I'm just another developer and tech
+      guy who starts late in the blog game. It could also stand for just 'The
+      Great' - singular or plural, depending on the technologies, programming
+      languages, frameworks, and people I will mention. That fits best!
     </p>
     <h2 class="font-headline font-bold mt-4">
       Sooo... Back to <s>the Future</s> basics
     </h2>
     <p class="mt-4">
-      Hi! I'm Alexander Classen, a passionate fullstacke developer from Germany
+      Hi! I'm Alexander Classen, a passionate full-stack developer from Germany,
       and this is my journey. The journey starts now. Fasten your seat belts and
       enjoy the ride!
     </p>
