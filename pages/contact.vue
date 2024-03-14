dfd186dd1b41132c964cf5932d5a0789225cc18d
Alexander Classen (Westwerk) <ac@westwerk.ac>
2024-03-14 20:18:07 +0100

    add contact button to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,9 +3,21 @@ definePageMeta({
   title: "Contact",
 });
 defineOgImageComponent("custom");
+const openTerminal = useState("terminal-open");
+const openTerminalCommand = useState("terminal-open-command");
 </script>
 <template>
   <div class="contact">
     <ContentDoc class="content" />
+    <BaseButton
+      class="mt-8 ml-4"
+      @click="
+        () => {
+          openTerminal = true;
+          openTerminalCommand = 'contact';
+        }
+      "
+      >Contact</BaseButton
+    >
   </div>
 </template>
