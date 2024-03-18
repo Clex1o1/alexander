eac6bc7d53be9ae8a3828b67b0a752fb2f935527
Alexander Classen (Westwerk) <ac@westwerk.ac>
2024-03-18 17:18:09 +0100

    fix: cookie

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 const { grantConsent } = useGtag();
 const cookies = useCookie("_ga", { maxAge: 60 * 60 * 24 * 30 });
-if (cookies.value !== "rejected") {
+if (cookies.value && cookies.value !== "rejected") {
   grantConsent();
 }
 </script>
