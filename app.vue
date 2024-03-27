00569aa806a6e0f237f987b62d99140f097e353e
Alexander Classen (Westwerk) <ac@westwerk.ac>
2024-03-27 14:55:49 +0100

    watch out for cookies

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
-const { grantConsent } = useGtag();
+const { grantConsent, revokeConsent } = useGtag();
 const cookies = useCookie("consent", { maxAge: 60 * 60 * 24 * 30 });
 watchEffect(() => {
   if (cookies.value && cookies.value !== "rejected") {
     grantConsent();
-  }
+  } else revokeConsent();
 });
 </script>
 <template>
