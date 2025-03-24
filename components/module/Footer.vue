97e2354063e894313f37c9bdae42c643ae44a148
Clex1o1 <alexander@the-great.dev>
2025-03-24 12:17:44 +0100

    feat: remove x for bluesky

--- components/module/Footer.vue
+++ components/module/Footer.vue
@@ -3,22 +3,28 @@
     <div class="border-t-2 border-amber-400 flex justify-between py-2 gap-16">
       <nav>
         <ul class="grid gap-1 *:flex">
-          <li><NuxtLink to="/imprint">Imprint</NuxtLink></li>
-          <li><NuxtLink to="/privacy">Privacy</NuxtLink></li>
-          <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+          <li>
+            <NuxtLink to="/imprint">Imprint</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/privacy">Privacy</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/contact">Contact</NuxtLink>
+          </li>
         </ul>
       </nav>
       <div class="text-right grid gap-1">
         <div class="flex gap-3 justify-end text-lg">
           <NuxtLink to="https://github.com/Clex1o1" title="GitHub">
-            <NuxtIcon name="github"
-          /></NuxtLink>
-          <NuxtLink to="https://twitter.com/Clex1o1" title="X/Twitter"
-            ><NuxtIcon name="x-twitter"
-          /></NuxtLink>
-          <NuxtLink to="https://de.linkedin.com/in/clex1o1" title="LinkedIn"
-            ><NuxtIcon name="linkedin"
-          /></NuxtLink>
+            <NuxtIcon name="github" />
+          </NuxtLink>
+          <NuxtLink to="https://bsky.app/profile/the-great.dev" title="BlueSky">
+            <NuxtIcon name="bluesky" />
+          </NuxtLink>
+          <NuxtLink to="https://de.linkedin.com/in/clex1o1" title="LinkedIn">
+            <NuxtIcon name="linkedin" />
+          </NuxtLink>
         </div>
         <p class="font-headline font-bold balance-text">
           Made with Nuxt, Tailwind and love
