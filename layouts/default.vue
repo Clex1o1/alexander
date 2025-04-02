baedf2682c052d09f62adca528edad1dfa96b69d
Clex1o1 <alexander@the-great.dev>
2025-04-02 09:46:59 +0200

    feat: structural improvements to fulfill semantic

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@ const slug = computed(() => route.fullPath);
 
 const { data, refresh } = await useAsyncData(
   "pages",
-  () => queryCollection('blog').path(route.fullPath).first(),
+  () => queryCollection("blog").path(route.fullPath).first(),
   {
     watch: [slug],
   }
@@ -23,27 +23,47 @@ const errorHappened = useState("errorHappened", () => false);
       </header>
       <div class="container mx-auto pt-8">
         <transition name="zapp-out" mode="out-in">
-          <h1 class="headline text-4xl text-wrap" :key="router.currentRoute.value.path">
-            {{ data?.title || router.currentRoute.value.meta.title || "&nbsp;" }}
+          <h1
+            class="headline text-4xl text-wrap"
+            :key="router.currentRoute.value.path"
+          >
+            {{
+              data?.title || router.currentRoute.value.meta.title || "&nbsp;"
+            }}
           </h1>
         </transition>
         <div class="intro grid gap-4 md:grid-cols-4 pt-8">
           <aside class="border-2 border-amber-400 p-4">
             <h2 class="font-headline font-bold">Menu</h2>
-            <nav aria-label="Main navigation" class="flex md:grid gap-y-1 gap-x-8 mt-4">
-              <NuxtLink class="nav-item" to="/home"><span class="underline">Home</span></NuxtLink>
-              <NuxtLink class="nav-item" to="/blog"><span class="underline">Blog</span></NuxtLink>
-              <NuxtLink class="nav-item" to="/about"><span class="underline">About</span></NuxtLink>
+            <nav
+              aria-label="Main navigation"
+              class="flex md:grid gap-y-1 gap-x-8 mt-4"
+            >
+              <NuxtLink class="nav-item" to="/home"
+                ><span class="underline">Home</span></NuxtLink
+              >
+              <NuxtLink class="nav-item" to="/blog"
+                ><span class="underline">Blog</span></NuxtLink
+              >
+              <NuxtLink class="nav-item" to="/about"
+                ><span class="underline">About</span></NuxtLink
+              >
             </nav>
           </aside>
-          <main class="border-2 border-amber-400 p-4 md:col-span-3 min-w-0" v-if="$slots.default">
+          <main
+            class="border-2 border-amber-400 p-4 md:col-span-3 min-w-0"
+            v-if="$slots.default"
+          >
             <ModuleLoading />
             <slot />
           </main>
         </div>
       </div>
       <ClientOnly>
-        <ModuleTerminal class="fixed bottom-0 inset-x-0" aria-label="Terminal interface"></ModuleTerminal>
+        <ModuleTerminal
+          class="fixed bottom-0 inset-x-0"
+          aria-label="Terminal interface"
+        ></ModuleTerminal>
       </ClientOnly>
       <footer>
         <slot name="footer">
